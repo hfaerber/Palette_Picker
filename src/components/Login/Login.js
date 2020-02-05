80e2ff7266425c0ae6b073440db831b237bd70f8
Cameron MacRae <cam.mac.cm@gmail.com>
2020-02-04 18:23:41 -0700

    Add error message

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,15 +6,29 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (validateSubmit()) {
+
+    } else {
+      setError('Both fields are required.');
+    }
+  };
+
+  const validateSubmit = () => {
+    return username && password ? true : false
+  }
+
   return (
     <div className="login">
       <h1>Palette Picker</h1>
       <span className="form-label">Please sign in to start pickin' palettes:</span>
-      <form className="login-form">
+      <form className="login-form" onSubmit={event => handleSubmit(event)}>
         <input className="username" type="text" placeholder="Username..." value={username} onChange={event => setUsername(event.target.value)}/>
         <input className="password" type="password" placeholder="Password..." value={password} onChange={event => setPassword(event.target.value)}/>
         <button type="submit" className="login-submit">Sign In</button>
       </form>
+      <span className="login-error">{error}</span>
     </div>
   )
 }
