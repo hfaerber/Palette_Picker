c5ae0ef632d3a0c32e2c9d1226dc65ed6f956096
Cameron MacRae <cam.mac.cm@gmail.com>
2020-02-05 16:21:51 -0700

    Add browser router and redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 import './App.scss';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="App">
       <Route exact path='/' render={() => <Login setUser={setUser}/>} />
+      <Route exact path='/home' render={() => <Home />} />
     </div>
   )
 }
