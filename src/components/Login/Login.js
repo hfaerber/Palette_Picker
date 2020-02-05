4c3a38eafb6ced3a23ae725f1e471e8dae12d879
Cameron MacRae <cam.mac.cm@gmail.com>
2020-02-04 17:57:43 -0700

    Add state hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
 
 const Login = () => {
-    return (
-      <div className="login">
-        <h1>Palette Picker</h1>
-        <span className="form-label">Please sign in to start pickin' palettes:</span>
-        <form className="login-form">
-          <input className="username" type="text" placeholder="Username..."/>
-          <input className="password" type="password" placeholder="Password..."/>
-          <button type="submit" className="login-submit">Sign In</button>
-        </form>
-      </div>
-    )
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  return (
+    <div className="login">
+      <h1>Palette Picker</h1>
+      <span className="form-label">Please sign in to start pickin' palettes:</span>
+      <form className="login-form">
+        <input className="username" type="text" placeholder="Username..." value={username} onChange={event => setUsername(event.target.value)}/>
+        <input className="password" type="password" placeholder="Password..." value={password} onChange={event => setPassword(event.target.value)}/>
+        <button type="submit" className="login-submit">Sign In</button>
+      </form>
+    </div>
+  )
 }
 
 export default Login;
