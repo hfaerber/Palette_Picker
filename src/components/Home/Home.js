2158f645ec83b0914bb64b6d5b3c6c39473e300d
Cameron MacRae <cam.mac.cm@gmail.com>
2020-02-07 14:43:15 -0700

    Add projects styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,6 +9,14 @@ const Home = props => {
         <h2>Palette Picker</h2>
         <Link className="sign-out" to="/" onClick={() => props.setUser({})}>Sign Out</Link>
       </nav>
+      <section className="project-section">
+        <div className="user-container">
+          <img className="profile-icon" src={require("../../images/icons/user.svg")}/>
+          <span className="username-label">{props.user}</span>
+        </div>
+        <div className="projects-container">
+        </div>
+      </section>
     </div>
   )
 }
