56ad3f9622dfff434d44b213b275ea91140b7a3c
Cameron MacRae <cam.mac.cm@gmail.com>
2020-02-07 23:31:47 -0700

    Add project post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,3 +9,19 @@ export const getProjects = async id => {
   const projects = await response.json();
   return projects
 };
+
+export const addProject = async name => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({name}),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  const response = await fetch('https://palette-picks.herokuapp.com/api/v1/projects', options);
+  if(!response.ok) {
+    throw new Error('Error: There was a problem adding your project.');
+  }
+  const project = await response.json();
+  return project
+};
