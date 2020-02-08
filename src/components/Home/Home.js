64e99dbf094fdfb435b9e8eb32479e5a80a08e92
Cameron MacRae <cam.mac.cm@gmail.com>
2020-02-07 23:31:47 -0700

    Add project post

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { addProject } from '../../apiCalls.js';
 import './Home.scss';
 
 const Home = props => {
   const [newProject, setNewProject] = useState('');
   const { user, setUser, projects, setProjects } = props;
+  const projectNodes = projects.map(project => {
+    return (
+      <div className="project-button" key={project.id} id={project.id}>
+        <img className="project-icon" src={require("../../images/icons/pantone.svg")}/>
+        <span className="project-name">{project.name}</span>
+      </div>
+    )
+  });
+
   return (
     <div className="home">
       <nav className="home-nav">
@@ -23,6 +33,9 @@ const Home = props => {
               <input className="add-project" type="text" placeholder="New Project" onChange={e => setNewProject(e.target.value)}/>
               <img className="add-icon" src={require("../../images/icons/add-button.svg")}/>
             </div>
+          </div>
+          <div className="small-project-wrapper">
+            {projectNodes}
           </div>
         </div>
       </section>
