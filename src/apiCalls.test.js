26d53366f5ca0ead56010758c9766f9dd6284306
Heather Faerber <heatherdfaerber@gmail.com>
2020-02-08 15:29:54 -0700

    Add tests for get projects fetch, get single project fetch and get palettes fetch

---
+++ src/apiCalls.test.js
@@ -0,0 +1,145 @@
+import { getProjects, addProject, getPalettes, getSpecificPalette } from './apiCalls';
+
+describe('getProjects', () => {
+
+  describe('getProjects for all projects', () => {
+    let mockResponse = { projects:
+     [ { id: 35,
+         name: 'Sample Project One',
+         created_at: '2020-02-05T00:52:52.258Z',
+         updated_at: '2020-02-05T00:52:52.258Z' },
+       { id: 36,
+         name: 'Sample Project Two',
+         created_at: '2020-02-05T00:52:52.261Z',
+         updated_at: '2020-02-05T00:52:52.261Z' } ] };
+
+    beforeEach(() => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => {
+            return Promise.resolve(mockResponse);
+          }
+        });
+      });
+    });
+
+    it('should be passed the correct URL', () => {
+      getProjects();
+      expect(window.fetch).toHaveBeenCalledWith(
+        'https://palette-picks.herokuapp.com/api/v1/projects'
+      );
+    });
+
+    it('should return an object with an array of projects', () => {
+      expect(getProjects()).resolves.toEqual(mockResponse);
+    });
+
+    it('should return an error for a response that is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        })
+      });
+      expect(getProjects()).rejects.toEqual(Error('Error: There was a problem getting your projects'));
+    });
+  });
+
+  describe('getProjects for a specific project', () => {
+    let mockId = 36;
+    let mockResponse = {
+      id: 36,
+      name: 'Sample Project Two',
+      created_at: '2020-02-05T00:52:52.261Z',
+      updated_at: '2020-02-05T00:52:52.261Z'
+    };
+
+    beforeEach(() => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => {
+            return Promise.resolve(mockResponse);
+          }
+        });
+      });
+    });
+
+    it('should be passed the correct URL', () => {
+      getProjects(mockId);
+      expect(window.fetch).toHaveBeenCalledWith(
+        'https://palette-picks.herokuapp.com/api/v1/projects/36'
+      );
+    });
+
+    it('should return a single project object', () => {
+      expect(getProjects(mockId)).resolves.toEqual(mockResponse);
+    });
+
+    it('should return an error for a response that is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        })
+      });
+      expect(getProjects(mockId)).rejects.toEqual(Error('Error: There was a problem getting your projects'));
+    });
+  });
+});
+
+describe('getPalettes', () => {
+  let mockResponse = { palettes:
+   [ { id: 6384,
+       name: 'Sample Palette one',
+       color_one: '#F7D951',
+       color_two: '#75B1FF',
+       color_three: '#985EEE',
+       color_four: '#5ED49B',
+       color_five: '#44638F',
+       projects_id: 3230,
+       created_at: '2020-02-08T00:59:46.352Z',
+       updated_at: '2020-02-08T00:59:46.352Z' },
+     { id: 6385,
+       name: 'Sample Palette two',
+       color_one: '#DC5551',
+       color_two: '#F2AD00',
+       color_three: '#6CA131',
+       color_four: '#4B5DC6',
+       color_five: '#1F0E95',
+       projects_id: 3230,
+       created_at: '2020-02-08T00:59:46.352Z',
+       updated_at: '2020-02-08T00:59:46.352Z' } ] };
+    let mockProjectId = 3230;
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => {
+          return Promise.resolve(mockResponse);
+        }
+      });
+    });
+  });
+
+  it('should be passed the correct URL', () => {
+    getPalettes(mockProjectId);
+    expect(window.fetch).toHaveBeenCalledWith(
+      'https://palette-picks.herokuapp.com/api/v1/projects/3230/palettes'
+    );
+  });
+
+  it('should return an object with an array of projects', () => {
+    expect(getPalettes(mockProjectId)).resolves.toEqual(mockResponse);
+  });
+
+  it('should return an error for a response that is not ok', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false
+      })
+    });
+    expect(getPalettes(mockProjectId)).rejects.toEqual(Error('Error: There was a problem getting the palettes for project id 3230'
+    ));
+  });
+})
