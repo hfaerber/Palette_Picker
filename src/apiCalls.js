0c58aa746da7b2b6a5eaf6624c3f67166ac69631
Heather Faerber <heatherdfaerber@gmail.com>
2020-02-09 09:03:15 -0700

    Add api call to post a palette

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,14 @@
+// HELPER FUNCTION
+const getRequestedData = async (url, errorMessage) => {
+  const response = await fetch(url);
+  if(!response.ok) {
+    throw new Error(`${errorMessage}`)
+  }
+  const data = await response.json();
+  return data
+};
+
+// API CALLS
 export const getProjects = async id => {
   const url = id ?
   `https://palette-picks.herokuapp.com/api/v1/projects/${id}` :
@@ -22,15 +33,6 @@ export const getSpecificPalette = async (paletteId) => {
   return getRequestedData(url, errorMessage);
 };
 
-const getRequestedData = async (url, errorMessage) => {
-  const response = await fetch(url);
-  if(!response.ok) {
-    throw new Error(`${errorMessage}`)
-  }
-  const data = await response.json();
-  return data
-};
-
 export const addProject = async name => {
   const options = {
     method: 'POST',
@@ -47,3 +49,20 @@ export const addProject = async name => {
   const id = await response.json();
   return id
 };
+
+export const addPalette = async (project_id, palette_body) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(palette_body),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  const response = await
+    fetch(`https://palette-picks.herokuapp.com/api/v1/projects/${project_id}/palettes`, options);
+  if(!response.ok) {
+    throw new Error('Error: There was a problem adding your palette.');
+  }
+  const id = await response.json();
+  return id
+}
