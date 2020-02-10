570d027180d420408f23faf28c05bd50b8df5fe9
Heather Faerber <heatherdfaerber@gmail.com>
2020-02-10 12:43:42 -0700

    Correct jest dependency and add snapshot for Login

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import { Login, handleChange } from './Login.js';
+import ReactDOM from 'react-dom';
+import { Login } from './Login.js';
+import { render, fireEvent } from '@testing-library/react';
 import { shallow } from 'enzyme';
 
 describe('Login', () => {
-  let wrapper, instance, mockEvent;
+  let wrapper, mockSetUser, mockError;
 
-  beforeEach(() => {
-    wrapper = shallow(<Login setUser={jest.fn()} />);
-    instance = wrapper.instance();
-    mockEvent = {target: {
-      id: 'username'
-    }};
-  });
-
-  it('Should match the snapshot', () => {
+  it('Should match snapshot', () => {
+    mockSetUser = jest.fn();
+    mockError = '';
+    wrapper = shallow(<Login
+      setUser={mockSetUser}
+      error={mockError}
+    />);
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('Should render without crashing', () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Login />, div);
+    ReactDOM.unmountComponentAtNode(div);
   });
-
-  it('Should invoke the handleSubmit method when there is a change on the username input', () => {
-    wrapper.find('#username').simulate('change', mockEvent);
-    expect(handleChange).toHaveBeenCalledWith('username');
+  it('Should load with initial username of an empty string', () => {
+    const { container } = render(<Login />);
+    const username = container.querySelector('#username');
+    expect(username.textContent).toBe('');
   });
-
-
+  it('Should load with an initial password of an empty string', () => {
+    const { container } = render(<Login />);
+    const password = container.querySelector('#password');
+    expect(password.textContent).toBe('');
+  });
+  it('Should load with an initial error of an empty string', () => {
+    const { container } = render(<Login />);
+    const error = container.querySelector('.login-error');
+    expect(error.textContent).toBe('');
+  });
+  it('Should add an error message if the button is clicked with no values in one or more inputs', () => {
+    const { container } = render(<Login />);
+    const button = container.querySelector('.login-submit');
+    const error = container.querySelector('.login-error');
+    fireEvent.click(button);
+    expect(error.textContent).toBe('Both fields are required.');
+  });
 });
