e384cac6678dbe26d9db4de48c63191f3e4e688e
Heather Faerber <heatherdfaerber@gmail.com>
2020-02-10 13:55:35 -0700

    Add snapshot test for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { shallow } from 'enzyme';
 
-describe.skip('App', () => {
-  it('', () => {});
-})
+describe('App', () => {
+  let wrapper;
+
+  it('Should match the snapshot', () => {
+    wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
