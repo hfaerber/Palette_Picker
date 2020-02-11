7bc1fec44487cea0d368cd9db45f386adc8aacc5
Cameron MacRae <cam.mac.cm@gmail.com>
2020-02-10 21:03:52 -0700

    Add and style loader

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import Loader from './components/Loader/Loader';
 import { getProjects } from './apiCalls.js';
 import './App.scss';
 
